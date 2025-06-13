3837ef745b477795a4dbe73ff982998d2db6eeb6
erin <botheritons2005@gmail.com>
2025-06-13 10:41:06 -0300

    fix: erro com tipo
    definindo o tipo padrao como null

--- src/Infrastructure/Persistence/Mysql/DatabaseInstance.php
+++ src/Infrastructure/Persistence/Mysql/DatabaseInstance.php
@@ -7,12 +7,12 @@ use PDO;
 class DatabaseInstance
 {
 
-  private ?PDO $instance;
+  private static ?PDO $instance = null;
   public static function getConnection()
   {
 
     if (self::$instance === null) {
-      self::$instance = new PDO("mysql:host=0.0.0.0;dbname=nekro_dm", "root", "100Senha#", [
+      self::$instance = new PDO("mysql:host=0.0.0.0:3307;dbname=nekro_dm", "root", "100Senha#", [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
       ]);
